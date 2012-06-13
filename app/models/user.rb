645a164e6690afe36527d0d1d140724a943594cd
Fatima <faabudeeb@hotmail.com>
2012-06-14 00:29:33 +0300

    adding valdation and static pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :gender, :last_name, :password
+  attr_accessible :email, :first_name, :gender, :last_name, :password
+  validates_presence_of :first_name ,:message =>" is required"
+  validates_presence_of :last_name ,:message =>" is required"
+   validates_presence_of :email,:message => " is required"
+  validates_presence_of :password,:message  => " is required"
+  validates_presence_of :gender , :message  => " is required"
+   validates_format_of  :password, :with => /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#%]).{6,20})/, :message => " dose not meet password requirment"
+  validates_format_of  :email, :with => /\b[A-Za-z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/ , message: " is not correct"
+
 end
