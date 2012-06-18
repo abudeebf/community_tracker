b7099084f2a0146a8cf73bbe8570579751eb13b8
Fatima <faabudeeb@hotmail.com>
2012-06-18 23:36:56 +0300

    gravitar

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,14 @@ module SessionsHelper
        self.current_user = user
        end
        def signed_in?
-       	!current_user.nil?
+         current_user.nil?
        end
        def current_user=(user)
        	@current_user=user
+       end
+       def sign_out
+              current_user=nil
+              cookies.delete(:remember_token)
        end
        def current_user
        	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
