046790b9742500aaab1307b3471d5710632ffbb4
Avishek Neupane <avishek@brandeis.edu>
2012-07-11 16:11:00 -0400

    fixed the regex stuff

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,16 +16,18 @@ class PasswordResetsController < ApplicationController
  end
  def update
  @user = User.find_by_password_reset_token!(params[:id])
-  if @user.password_reset_sent_at < 2.hours.ago
-    redirect_to new_password_reset_path, :alert => "Password &crarr; 
+ regexp=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/
+ if regexp.match (params[:user][:password]) 
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password &crarr; 
       reset has expired."
       @user.audit_comment="reset password"
-  elsif @user.update_attributes(params[:user])
-    redirect_to root_url, :notice => "Password has been reset."
-  else
-    render :edit
+    elsif @user.update_attributes(params[:user])
+      redirect_to root_url, :notice => "Password has been reset."
+    end
+    else
+      flash[:error]="password does not meet requirments"
+      render :edit
+    end
   end
-
-
-end
 end
