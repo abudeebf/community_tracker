7a7d7a7760bccf808dfdba9f1a824a4cc853b80f
Fatima <faabudeeb@hotmail.com>
2012-07-16 23:37:50 +0300

    eeeee

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -92,7 +92,7 @@ end
   def confirm_participants
     @event=Event.find(params[:event].to_i)
   
-   for i in (0..(params[:users].length -1))
+   for i in 0..params[:users].length
      @participation=Participation.find(:all, :conditions => [ "event_id = ? and user_id=?", (params[:event]).to_i,(params[:users][i]).to_i])
      if (params[:attend][i]=="true")
       @participation[0].attend=true
