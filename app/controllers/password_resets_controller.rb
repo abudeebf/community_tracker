bd71db9ed374c6481af2dc0a1ceb524ff51833ef
kashive <avishek@brandeis.edu>
2012-08-01 11:36:17 -0400

    changed the password regex

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,7 +16,7 @@ class PasswordResetsController < ApplicationController
  end
  def update
  @user = User.find_by_password_reset_token!(params[:id])
- regexp=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/
+ regexp=/((?=.*\d)(?=.*[a-z]).{6,20})/
  if regexp.match (params[:user][:password]) 
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password &crarr; 
